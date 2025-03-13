c7a1c84a238079f961a1f1ecdd5039daeb06e4da
Greg Walker <git@greg.email>
2025-03-13 09:48:13 -0500

    show room avatars

--- src/app/pages/client/space/Space.tsx
+++ src/app/pages/client/space/Space.tsx
@@ -346,6 +346,8 @@ export function Space() {
   const getToLink = (roomId: string) =>
     getSpaceRoomPath(spaceIdOrAlias, getCanonicalAliasOrRoomId(mx, roomId));
 
+  const [roomAvatars] = useSetting(settingsAtom, 'roomAvatars');
+
   return (
     <PageNav>
       <SpaceHeader />
@@ -425,7 +427,7 @@ export function Space() {
                   <RoomNavItem
                     room={room}
                     selected={selectedRoomId === roomId}
-                    showAvatar={mDirects.has(roomId)}
+                    showAvatar={mDirects.has(roomId) || roomAvatars}
                     direct={mDirects.has(roomId)}
                     linkPath={getToLink(roomId)}
                     notificationMode={getRoomNotificationMode(notificationPreferences, room.roomId)}
