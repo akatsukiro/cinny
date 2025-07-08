1647de55c4d0515e58bc258b36de496bf3b2090d
Kris Hu <i@krishu.moe>
2025-07-08 16:34:51 +0800

    refine notification format

--- src/app/pages/client/ClientNonUIFeatures.tsx
+++ src/app/pages/client/ClientNonUIFeatures.tsx
@@ -164,10 +164,10 @@ function MessageNotifications() {
       eventId: string;
       message: string;
     }) => {
-      const noti = new window.Notification(roomName, {
+      const noti = new window.Notification(`${username} in ${roomName}`, {
         icon: roomAvatar,
         badge: roomAvatar,
-        body: `From ${username}: ${message}`,
+        body: message,
         silent: true,
       });
 
--- src/sw.ts
+++ src/sw.ts
@@ -145,14 +145,17 @@ const handlePushNotificationEventData = async (eventData: PushMessageData) => {
     await navigator.clearAppBadge();
   }
 
-  const title = pushData.room_name ?? "New Notification";
-  const sender_display_name = pushData.sender_display_name ?? "Unknown";
+  let title = undefined;
+  if (pushData.sender_display_name && pushData.room_name) {
+    title = `${pushData.sender_display_name} in ${pushData.room_name}`;
+  }
+  title = title ?? "New Notification";
 
   let body = "You have a new message";
   if (pushData.content.ciphertext) {
-    body = `Encrypted message from ${sender_display_name}`
+    body = `Encrypted message`;
   } else if (pushData.content.body) {
-    body = `From ${sender_display_name}: ${pushData.content.body}`;
+    body = pushData.content.body;
   } else {
     return;
   }
