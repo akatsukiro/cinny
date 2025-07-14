4608b8d6002659d56f709d680a7e67c8730dd10f
Kris Hu <i@krishu.moe>
2025-07-14 17:45:58 +0800

    fix(to): load space list first

--- src/app/pages/client/ToRoomEvent.tsx
+++ src/app/pages/client/ToRoomEvent.tsx
@@ -1,16 +1,31 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useRoomNavigate } from "../../hooks/useRoomNavigate";
+import { useMatrixClient } from "../../hooks/useMatrixClient";
+import { useAtomValue } from "jotai";
+import { roomToParentsAtom } from "../../state/room/roomToParents";
+import { mDirectAtom } from "../../state/mDirectList";
 
 export function ToRoomEvent() {
+  const mx = useMatrixClient();
+  const roomToParents = useAtomValue(roomToParentsAtom);
+  const mDirects = useAtomValue(mDirectAtom);
   const { room_id, event_id } = useParams();
   const { navigateRoom } = useRoomNavigate();
 
   useEffect(() => {
-    if (!room_id) { return; }
+    if (!room_id || !mx) { return; }
+    if (!roomToParents.size || !mDirects.size) { return; }
 
     navigateRoom(room_id, event_id);
-  }, [room_id, event_id, navigateRoom]);
+  }, [
+    mx,
+    roomToParents,
+    mDirects,
+    room_id,
+    event_id,
+    navigateRoom
+  ]);
 
   return null;
 }
